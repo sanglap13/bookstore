7ebc0d9ee1a1530c1f71f88f4c0f4bd5333aff34
sanglap13 <sanglapmridha@gmail.com>
2023-09-30 18:41:54 +0530

    added context api

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { About, Home } from "./components/pages";
 import { BookDetails, BookList } from "./components/shared";
+import { AppProvider } from "./context/context";
 
 const App = () => {
   return (
     <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="/about" element={<About />} />
-            <Route path="/book" element={<BookList />} />
-            <Route path="/book/:id" element={<BookDetails />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AppProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="/about" element={<About />} />
+              <Route path="/book" element={<BookList />} />
+              <Route path="/book/:id" element={<BookDetails />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppProvider>
     </div>
   );
 };

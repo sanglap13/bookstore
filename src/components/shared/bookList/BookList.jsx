9129914a01acc2267e033582fd78627d290a66d0
sanglap13 <sanglapmridha@gmail.com>
2023-10-03 19:26:59 +0530

    feat: loader and booklist

--- src/components/shared/bookList/BookList.jsx
+++ src/components/shared/bookList/BookList.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useGlobalContext } from "../../../context/context";
+import coverImg from "../../../assets/cover_not_found.jpg";
+import Loader from "../loader/Loader";
+import Book from "./Book";
+
+import "./bookList.css";
 
 const BookList = () => {
-  return <div>BookList</div>;
+  const { books, loading, resultTitle } = useGlobalContext();
+  const booksWithCovers = books.map((singleBook) => {
+    return {
+      ...singleBook,
+      // removing /works/ to get only id
+      id: singleBook.id.replace("/works/", ""),
+      cover_img: singleBook.cover_id
+        ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg`
+        : coverImg,
+    };
+  });
+
+  if (loading) return <Loader />;
+
+  return (
+    <section className="booklist">
+      <div className="booklist-container">
+        <div className="booklist-section-title">
+          <h2>{resultTitle}</h2>
+        </div>
+        <div className="booklist-section-content">
+          {booksWithCovers.slice(0, 30).map((item, index) => {
+            return <Book key={index} {...item} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BookList;
--- src/components/shared/loader/Loader.jsx
+++ src/components/shared/loader/Loader.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import LoaderImg from "../../../assets/loader.svg";
+
+import "./loader.css";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="loader">
+      <img src={LoaderImg} alt="loader" />
+    </div>
+  );
 };
 
 export default Loader;

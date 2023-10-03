1a96aa682c66c5487c2c494a684d4c4c8f887e6f
sanglap13 <sanglapmridha@gmail.com>
2023-10-03 20:10:43 +0530

    refactor: change booklist

--- src/components/shared/bookList/Book.jsx
+++ src/components/shared/bookList/Book.jsx
@@ -5,29 +5,28 @@ import "./bookList.css";
 
 const Book = (book) => {
   return (
-    <div className="book-item flex flex-column flex-sb">
+    <div className="book">
       <div className="book-item-img">
         <img src={book.cover_img} alt="cover" />
       </div>
-      <div className="book-item-info text-center">
+      <div className="book-item-info">
         <Link to={`/book/${book.id}`} {...book}>
-          <div className="book-item-info-item title fw-7 fs-18">
+          <div className="book-item-info-title item">
             <span>{book.title}</span>
           </div>
         </Link>
-
-        <div className="book-item-info-item author fs-15">
-          <span className="text-capitalize fw-7">Author: </span>
+        <div className="book-item-info-author item">
+          <span className="book-item-info-author-label">Author: </span>
           <span>{book.author.join(", ")}</span>
         </div>
-
-        <div className="book-item-info-item edition-count fs-15">
-          <span className="text-capitalize fw-7">Total Editions: </span>
+        <div className="book-item-info-edition item">
+          <span className="book-item-info-edition-label">Total Editions: </span>
           <span>{book.edition_count}</span>
         </div>
-
-        <div className="book-item-info-item publish-year fs-15">
-          <span className="text-capitalize fw-7">First Publish Year: </span>
+        <div className="book-item-info-publish-year item">
+          <span className="book-item-info-publish-year-label">
+            First Publish Year:{" "}
+          </span>
           <span>{book.first_publish_year}</span>
         </div>
       </div>
